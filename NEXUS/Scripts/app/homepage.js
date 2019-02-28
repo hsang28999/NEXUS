641f3b882c737ebe9fa1ed4530115c8fd42aea0e
DaoTuanTu <tudt00552@fpt.edu.vn>
2019-02-28 08:53:07 +0700

    update #2
    up lai file

---
+++ NEXUS/Scripts/app/homepage.js
@@ -0,0 +1,21 @@
+﻿$("#modaldis-login").click(function () {
+    window.setTimeout(function () {
+        $("#loginmodal").modal('hide');
+    }, 1000);
+    window.setTimeout(function () {
+        $("#registermodal").modal('show');
+    }, 1000);
+    //$("#loginmodal").modal('hide');
+    //$("#registermodal").modal('show');
+});
+
+$("#modaldis-res").click(function () {
+    window.setTimeout(function () {
+        $("#registermodal").modal('hide');
+    }, 1000);
+    window.setTimeout(function () {
+        $("#loginmodal").modal('show');
+    }, 1000);
+    //$("#registermodal").modal('hide');
+    //$("#loginmodal").modal('show');
+});
