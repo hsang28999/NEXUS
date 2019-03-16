88e3865a494a4febda3e580ab5608cd399c0f4f4
hsang28999 <sanghd00541@fpt.edu.vn>
2019-03-16 22:55:52 +0700

    update admin

---
+++ NEXUS/Scripts/admin/userctrl.js
@@ -0,0 +1,12 @@
+﻿function UserAPCtrl($scope, $rootScope, $stateParams, $location, $timeout, xhrService, $anchorScroll) {
+    $scope.loadUserAP = function () {
+        xhrService.get("GetListUser/1").then(function (data) {
+            $scope.AllUserAp = data.data.data;
+            console.log(data.data.data.length);
+        }, function (error) {
+           
+        });
+    } 
+}
+
+app.controller('UserAPCtrl', UserAPCtrl);
