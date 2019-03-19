431bde640c31509de03e8a2901378596a7b11fd6
DaoTuanTu <tudt00552@fpt.edu.vn>
2019-03-19 23:35:25 +0700

    update add product, create contract, view history ...

---
+++ NEXUS/Scripts/admin/contractdetail.js
@@ -0,0 +1,144 @@
+﻿function ContractdetailAPCtrl($scope, $rootScope, $stateParams, $location, $timeout, xhrService, $anchorScroll) {
+    var id = $stateParams.id;
+    myEl = angular.element(document.querySelector('.header2'));
+    toastr.options = {
+        "positionClass": "toast-bottom-right",
+    }
+
+    $scope.loadContractdetailAP = function () {
+
+        $scope.AllStatus = [
+            { value: 1, name: "Activated" },
+            { value: 2, name: "	Not Activated" }
+        ];
+
+        xhrService.get("GetListUser/1").then(function (data) {
+            $scope.AllUser = data.data.data;
+        }, function (error) {
+
+        });
+
+        xhrService.get("GetListProductAdmin/1").then(function (data) {
+            $scope.AllProduct = data.data.data;
+        }, function (error) {
+
+        });
+
+        xhrService.get("GetListEmployee/1").then(function (data) {
+            $scope.AllEmployee = data.data.data;
+        }, function (error) {
+
+        });
+
+        xhrService.get("GetListStore").then(function (data) {
+            $scope.AllStore = data.data;
+        }, function (error) {
+
+        });
+
+
+        $scope.AllType = [
+            { value: 1, name: "Prepaid By Hour" },
+            { value: 2, name: "Unlimited Prepaid" },
+            { value: 3, name: "Postpaid" }
+        ];
+
+        xhrService.get("GetListConnectionGroup").then(function (data) {
+            $scope.AllGroup = data.data;
+        }, function (error) {
+
+        });
+
+        if (id == undefined || id == null || id == "") {
+            $scope.start = new Date();
+            $scope.btnUpdate = true;
+            $scope.inpHide = true;
+            $scope.btnAdd = false;
+            myEl.html("New Contract");
+        }
+        else {
+            $scope.btnAdd = true;
+            $scope.disuser = true;
+            $scope.btnUpdate = false;
+            myEl.html("Edit Contract");          
+            xhrService.get("GetContractDetail/" + id).then(function (data) {
+                $scope.user = data.data.UserId;
+                $scope.employee = data.data.EmployeeId;
+                $scope.product = data.data.ProductId;
+                $scope.store = data.data.StoreId;
+                $scope.note = data.data.Note;
+                $scope.signing = new Date(data.data.SigningDate * 1000);
+                $scope.status = data.data.Status;
+                $scope.price = data.data.Price;
+                $scope.start = new Date(data.data.StartDate * 1000);
+                $scope.end = new Date(data.data.EndDate * 1000);
+                $scope.type = data.data.ProductType;
+                $scope.time = data.data.ProductTimeUsed;
+                $scope.local = data.data.PpmLocal;
+                $scope.std = data.data.PpmStd;
+                $scope.mobile = data.data.PpmMobile;
+                $scope.address = data.data.Address;
+                $scope.sec = data.data.SecurityDeposit;
+                $scope.used = data.data.TimeUsedAvailable;
+            }, function (error) {
+
+            });
+        }
+
+        
+    }
+
+    $scope.Update = function () {
+        var dataSend = {
+            "Note": $scope.note,
+            "Status": $scope.status
+        }
+        xhrService.get("GetContractDetail/" + id).then(function (data) {
+            xhrService.post("SaveContract/" + id, dataSend).then(function (data) {
+                toastr.success('Save done !', 'Success');
+                setTimeout(() => {
+                    window.location.href = "admin/contract";
+                }, 2000);
+            }, function (error) {
+                toastr.error(error.statusText, 'Error');
+            });
+        }, function (error) {
+            toastr.error(error.statusText, 'Error');
+        });
+    }
+
+    $scope.Add = function () {
+        var date = new Date($scope.start).getTime() / 1000;
+        var dataSend = {
+            "Status": 1,
+            "StartDate": date,
+            "Price": $scope.price,
+            "Note": $scope.note,
+            "StoreId": $scope.store,
+            "EmployeeId": $scope.employee,
+            "UserId": $scope.user,
+            "ProductId": $scope.product
+        };
+        if ($scope.price == undefined || $scope.note == undefined
+            || $scope.store == undefined || $scope.employee == undefined || $scope.user == undefined || $scope.product == undefined) {
+            $scope.msgErr = true;
+        }
+        else {
+            $scope.msgErr = false;
+            xhrService.post("CreateContract", dataSend).then(function (data) {
+                toastr.success('Add new contract done !', 'Success');
+                setTimeout(() => {
+                    window.location.href = "admin/contract";
+                }, 2000);
+            }, function (error) {
+                toastr.error(error.statusText, 'Error');
+            });
+        }
+    }
+
+    $scope.Delete = function () {
+        toastr.error("dAnG phAt tRiEn ?", 'Loi');
+    }
+}
+
+app.controller('ContractdetailAPCtrl', ContractdetailAPCtrl);
---
+++ NEXUS/Scripts/admin/productdetail.js
@@ -0,0 +1,123 @@
+﻿function ProductdetailAPCtrl($scope, $rootScope, $stateParams, $location, $timeout, xhrService, $anchorScroll) {
+    var id = $stateParams.id;
+    myEl = angular.element(document.querySelector('.header2'));
+    toastr.options = {
+        "positionClass": "toast-bottom-right",
+    }
+
+    $scope.loadProductdetailAP = function () {
+
+        $scope.AllType = [
+            { value: 1, name: "Prepaid By Hour"},
+            { value: 2, name: "Unlimited Prepaid" },
+            { value: 3, name: "Postpaid" }
+        ];
+
+        xhrService.get("GetListConnectionGroup").then(function (data) {
+            $scope.AllGroup = data.data;
+        }, function (error) {
+
+            });
+
+        if (id == undefined || id == null || id == "") {
+            $scope.start = new Date();
+            $scope.btnUpdate = true;
+            $scope.btnAdd = false;
+            myEl.html("New Contract");
+        }
+        else {
+            $scope.btnAdd = true;
+            $scope.disuser = true;
+            $scope.btnUpdate = false;
+            myEl.html("Edit Product");
+            xhrService.get("GetProductDetail/" + id).then(function (data) {
+                console.log(data.data.ConnectionGroupId);
+                $scope.name = data.data.ProductName;
+                $scope.des = data.data.Description;
+                $scope.price = data.data.Price;
+                $scope.type = data.data.TimeType;
+                $scope.time = data.data.TimeType;
+                $scope.timeused = data.data.TimeUsed;
+                $scope.ppmlocal = data.data.PpmLocal;
+                $scope.ppmstd = data.data.PpmStd;
+                $scope.ppmmobile = data.data.PpmMobile;
+                $scope.status = data.data.Status;
+                $scope.group = data.data.ConnectionGroupId;
+                $scope.month = data.data.MonthAvailable;
+            }, function (error) {
+
+            });
+        }
+    }
+
+    $scope.Update = function () {
+        console.log($scope.type);
+        var gend = document.getElementById('gender');
+        var dataSend = {
+            "ConnectionGroupId": $scope.group,
+            "TimeType": $scope.type,
+            "PpmMobile": $scope.ppmmobile,
+            "PpmStd": $scope.ppmstd,
+            "TimeUsed": $scope.timeused,
+            "Type": 1,
+            "Description": $scope.des,
+            "MonthAvailable": $scope.month,
+            "PpmLocal": $scope.ppmlocal,
+            "Price": $scope.price,
+            "ProductName": $scope.name,
+            "Status": 1
+        }
+        xhrService.get("GetProductDetail/" + id).then(function (data) {
+            xhrService.post("SaveProductDetail/" + id, dataSend).then(function (data) {
+                toastr.success('Save done !', 'Success');
+                setTimeout(() => {
+                    window.location.href = "admin/product";
+                }, 2000);
+            }, function (error) {
+                toastr.error(error.statusText, 'Error');
+            });
+        }, function (error) {
+            toastr.error(error.statusText, 'Error');
+        });
+    }
+
+    $scope.Add = function () {
+        var date = new Date($scope.start).getTime() / 1000;
+        var dataSend = {
+            "ConnectionGroupId": $scope.group,
+            "TimeType": $scope.type,
+            "PpmMobile": $scope.ppmmobile,
+            "PpmStd": $scope.ppmstd,
+            "TimeUsed": $scope.timeused,
+            "Type": 1,
+            "Description": $scope.des,
+            "MonthAvailable": $scope.month,
+            "PpmLocal": $scope.ppmlocal,
+            "Price": $scope.price,
+            "ProductName": $scope.name,
+            "Status": 1
+        };
+        if ($scope.group == undefined || $scope.type == undefined
+            || $scope.timeused == undefined || $scope.des == undefined || $scope.month == undefined
+            || $scope.price == undefined || $scope.name == undefined) {
+            $scope.msgErr = true;
+        }
+        else {
+            $scope.msgErr = false;
+            xhrService.post("CreateProductDetail", dataSend).then(function (data) {
+                toastr.success('Add new product done !', 'Success');
+                setTimeout(() => {
+                    window.location.href = "admin/product";
+                }, 2000);
+            }, function (error) {
+                toastr.error(error.statusText, 'Error');
+            });
+        }
+    }
+
+    $scope.Delete = function () {
+        toastr.error("dAnG phAt tRiEn ?", 'Loi');
+    }
+}
+
+app.controller('ProductdetailAPCtrl', ProductdetailAPCtrl);
